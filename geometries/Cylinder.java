ec78e6e8b890c3ef7c6cee84656604ebc1685549
Shoham Zeharya <shozeh123@gmail.com>
2022-03-28 19:35:44 +0300

    Refactored - needed Cylinder to inherit from Tube

--- geometries/Cylinder.java
+++ geometries/Cylinder.java
@@ -6,40 +6,24 @@ import primitives.*;
  * Cylinder class represents a three-dimensional Cylinder in 3D Cartesian coordinate
  * system
  */
-public class Cylinder implements Geometry{
-    private final Ray dir;
-    private final double radius;
+public class Cylinder extends Tube implements Geometry {
     private final double height;
 
     /**
      * Constructor for Cylinder using an axis ray, radius and height
-     * @param direction the axis ray
+     *
+     * @param ray    the axis ray
      * @param radius the radius
      * @param height the height
      */
-    public Cylinder(Ray direction, double radius, double height) {
-        this.dir = direction;
-        this.radius = radius;
+    public Cylinder(Ray ray, double radius, double height) {
+        super(ray, radius);
         this.height = height;
-    }
-    /**
-     * Getter for the axis ray of the Cylinder
-     * @return the axis ray
-     */
-    public Ray getDirection() {
-        return dir;
-    }
-
-    /**
-     * Getter for the radius of the Cylinder
-     * @return the radius
-     */
-    public double getRadius() {
-        return radius;
     }
 
     /**
      * Getter for the height of the Cylinder
+     *
      * @return the height
      */
     public double getHeight() {
@@ -48,17 +32,17 @@ public class Cylinder implements Geometry{
 
     @Override
     public Vector getNormal(Point point) {
-        Vector dir = this.dir.getDirection();
-        Point P0 = this.dir.getStart();
-        if(point.equals(P0)){
-            return this.dir.getDirection().scale(-1);
+        Vector dir = this.getDirection().getDirection();
+        Point P0 = this.getDirection().getStart();
+        if (point.equals(P0)) {
+            return dir.scale(-1);
         }
         double t = dir.dotProduct(point.subtract(P0));
-        if(t == 0){
-            return this.dir.getDirection().scale(-1);
+        if (t == 0) {
+            return dir.scale(-1);
         }
-        if(t == this.height){
-            return this.dir.getDirection();
+        if (t == this.height) {
+            return dir;
         }
         Point O = P0.add(dir.scale(t));
         Vector normal = point.subtract(O);

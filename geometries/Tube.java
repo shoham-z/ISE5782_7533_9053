5d35262f9c57490d168805caba1638fc0ab3d81e
Shoham <shozeh123@gmail.com>
2022-05-03 13:04:34 +0300

    fixed intersection implementation

--- geometries/Tube.java
+++ geometries/Tube.java
@@ -4,6 +4,7 @@ import primitives.*;
 
 import java.util.List;
 
+import static primitives.Util.alignZero;
 import static primitives.Util.isZero;
 
 /**
@@ -55,22 +56,39 @@ public class Tube extends Geometry {
 
     @Override
     public List<GeoPoint> findGeoIntersectionsHelper(Ray ray) {
-        if(ray.equals(this.axis)) return null;
-        Vector v = this.axis.getDirection();
-        Vector u = ray.getDirection();
-        double uv = u.dotProduct(v);
-        if(uv == 0) return null;
-        Vector temp = v.scale(uv);
-        if (u.equals(temp)) return null;
+        if (ray.equals(this.axis)) return null;
+        Vector tubeAxis = this.axis.getDirection();
+        Vector rayAxis = ray.getDirection();
+        double uv = alignZero(rayAxis.dotProduct(tubeAxis));
+        double a;
+        double b;
         Vector deltaP = ray.getStart().subtract(this.axis.getStart());
+        double deltaPv = alignZero(deltaP.dotProduct(tubeAxis));
+        if (deltaPv == 0) return null;
+        Vector w = tubeAxis.scale(deltaPv);
+        if (w.equals(deltaP)) return null;
+        Vector later = deltaP.subtract(w);
+        if (uv == 0) {
+            a = alignZero(rayAxis.lengthSquared());
+            b = alignZero(2 * rayAxis.dotProduct(later));
+        } else {
+            Vector temp = tubeAxis.scale(uv);
+            if (rayAxis.equals(temp)) return null;
 
-        double a = u.subtract(temp).lengthSquared();
-        double b = 2 * u.subtract(temp).dotProduct(deltaP.subtract(v.scale(deltaP.dotProduct(v))));
-        double c = deltaP.subtract(v.scale(deltaP.dotProduct(v))).lengthSquared() - this.radius * radius;
-        double discriminant = b * b - 4 * a * c;
+            a = alignZero(rayAxis.subtract(temp).lengthSquared());
+            b = alignZero(2 * rayAxis.subtract(temp).dotProduct(later));
+        }
 
-        if (discriminant < 0) return null;
-        if (discriminant==0) return List.of (new GeoPoint(this,ray.getPoint(-b/2*a)));
-        return List.of (new GeoPoint(this,ray.getPoint((-b + discriminant)/2*a)),new GeoPoint(this,ray.getPoint((-b-discriminant)/2*a)));
+        double c = alignZero(later.lengthSquared() - this.radius * this.radius);
+        double discriminant2 = alignZero(b * b - 4 * a * c);
+
+        if (discriminant2 < 0) return null;
+        if (discriminant2 == 0) return List.of(new GeoPoint(this, ray.getPoint(-b / 2 * a)));
+        double discriminant = Math.sqrt(discriminant2);
+        double t1 = alignZero((-b + discriminant) / 2 * a);
+        if (t1 < 0) return null;
+        double t2 = alignZero((-b - discriminant) / 2 * a);
+        if (t2 < 0) return List.of(new GeoPoint(this, ray.getPoint(t1)));
+        return List.of(new GeoPoint(this, ray.getPoint(t1)), new GeoPoint(this, ray.getPoint(t2)));
     }
 }

0b7e72a5732f70c29374eb52bb846e1d4dfc6734
eliyahu <horowitzeliyahu@gmail.com>
2022-05-09 20:43:58 +0300

    finish tube test

--- geometries/Tube.java
+++ geometries/Tube.java
@@ -97,9 +97,9 @@ public class Tube extends Geometry {
         double t2 = alignZero((-b - sDelta) / (2 * a));
         if (t1 <= 0) return null;
         List<GeoPoint> intersections = new LinkedList<>();
+        if (t2 > 0 && alignZero(t2 - maxDistance) <= 0) intersections.add(new GeoPoint(this, rayStart.add(rayDir.scale(t2))));
         if (alignZero(t1 - maxDistance) <= 0)
             intersections.add(new GeoPoint(this, t1 == 0 ? rayStart : rayStart.add(rayDir.scale(t1))));
-        if (t2 > 0 && alignZero(t2 - maxDistance) <= 0) intersections.add(new GeoPoint(this, rayStart.add(rayDir.scale(t2))));
         return intersections;
 
     }
